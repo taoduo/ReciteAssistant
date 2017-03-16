7d441d558a8632e1ee989ccd919bf59c0766c70d
Duo Tao <duo.tao@ligo.org>
2017-03-16 13:38:40 -0500

    Set up, get ready for latex

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,15 @@
 package sample;
 
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
 public class Controller {
+    @FXML
+    private TextField latexInput;
+
+    @FXML
+    public void addBtnClick() {
+        System.out.println(latexInput.getText());
+    }
 }
