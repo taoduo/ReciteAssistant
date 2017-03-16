6ebd079a234963076f87db6565014fefa74daebf
Duo Tao <duo.tao@ligo.org>
2017-03-16 18:57:34 -0500

    build adding flashcard UI

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -6,11 +6,12 @@ import org.scilab.forge.jlatexmath.TeXConstants;
 import org.scilab.forge.jlatexmath.TeXFormula;
 import org.scilab.forge.jlatexmath.TeXIcon;
 
-import java.awt.*;
+import java.awt.Graphics2D;
+import java.awt.Insets;
+import java.awt.Color;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
-
 
 import javax.imageio.ImageIO;
 import javax.swing.*;
@@ -18,42 +19,67 @@ import javax.swing.*;
 import javafx.embed.swing.SwingFXUtils;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
-import javafx.scene.control.TextField;
-import javafx.scene.image.ImageView;
+import javafx.scene.control.TextArea;
+import javafx.scene.image.*;
 
 public class Controller {
     @FXML
-    private TextField latexInput;
+    private TextArea latexHint;
 
     @FXML
-    private ImageView latexPreview;
+    private TextArea latexAnswer;
 
     @FXML
-    public void addBtnClick() {
-        String latex = latexInput.getText();
+    private ImageView hintPreview;
+
+    @FXML
+    private ImageView answerPreview;
+
+    @FXML
+    public void previewBtnClick() {
+        refreshHint();
+        refreshAnswer();
+    }
+
+    @FXML
+    public void saveBtnClick() {
+//        File file = new File("Example1.png");
+//        try {
+//            ImageIO.write(image, "png", file.getAbsoluteFile());
+//        } catch (IOException ex) { }
+    }
+
+    private void refreshHint() {
+        String latex = latexHint.getText();
+        hintPreview.setImage(getImageFromLatex(latex));
+    }
+
+    private void refreshAnswer() {
+        String latex = latexAnswer.getText();
+        answerPreview.setImage(getImageFromLatex(latex));
+    }
+
+    private Image getImageFromLatex(String latex) {
+        Image image = null;
         try {
             TeXFormula formula = new TeXFormula(latex);
             TeXIcon icon = formula.new TeXIconBuilder().setStyle(TeXConstants.STYLE_DISPLAY).setSize(20).build();
             icon.setInsets(new Insets(5, 5, 5, 5));
-            BufferedImage image = new BufferedImage(icon.getIconWidth(), icon.getIconHeight(), BufferedImage.TYPE_INT_ARGB);
-            Graphics2D g2 = image.createGraphics();
+            BufferedImage bufferedImage = new BufferedImage(icon.getIconWidth(), icon.getIconHeight(), BufferedImage.TYPE_INT_ARGB);
+            Graphics2D g2 = bufferedImage.createGraphics();
             g2.setColor(Color.white);
             g2.fillRect(0, 0, icon.getIconWidth(), icon.getIconHeight());
             JLabel jl = new JLabel();
             jl.setForeground(new Color(0, 0, 0));
             icon.paintIcon(jl, g2, 0, 0);
-            latexPreview.setImage(SwingFXUtils.toFXImage(image, null));
+            image = SwingFXUtils.toFXImage(bufferedImage, null);
         } catch (ParseException e) {
             showErrorAlert(e.getClass().toString(), e.getMessage());
         }
-//        File file = new File("Example1.png");
-//        try {
-//            ImageIO.write(image, "png", file.getAbsoluteFile());
-//        } catch (IOException ex) { }
-
+        return image;
     }
 
-    private static void showErrorAlert(String title, String content) {
+    private void showErrorAlert(String title, String content) {
         Alert alert = new Alert(Alert.AlertType.ERROR);
         alert.setTitle("Error Message");
         alert.setHeaderText(title);
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -11,8 +11,8 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 600, 400));
+        primaryStage.setTitle("Recite Assistant");
+        primaryStage.setScene(new Scene(root, 800, 600));
         primaryStage.setResizable(false);
         primaryStage.show();
     }
